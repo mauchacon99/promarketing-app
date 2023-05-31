978b1610ef1dd1f0f8da89b5e2f0e3d447562ea6
Francisco Aranda <francisco.aranda@sudo.cl>
2023-05-31 11:41:01 -0400

    chore: implements double quotes for typescript-eslint

--- apps/webs/latamwin/app/page.tsx
+++ apps/webs/latamwin/app/page.tsx
@@ -1,18 +1,18 @@
-import { Button, Header, Card } from 'ui-latamwin';
+import { Button, Header, Card } from "ui-latamwin";
 /**
  * Descripión.
  *
  * @returns JSX.Element.
  */
 export default function page() {
-	return (
-		<>
-			<div>
-				<Header className="bg-primary-default" />
-				<Button className={''} />
-				<Header className="pedro" />
-				<Card />
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div>
+        <Header className="bg-primary-default" />
+        <Button className={""} />
+        <Header className="pedro" />
+        <Card />
+      </div>
+    </>
+  );
 }
