0a73dfc2100158170e87bead83d83695796c7218
mauchaconrithmXO <Mauricio.Chacon@rithmXO.com>
2023-06-02 14:24:01 -0400

    fix: path cva button

--- packages/ui/ui-latamwin/src/components/common/button/button.interface.ts
+++ packages/ui/ui-latamwin/src/components/common/button/button.interface.ts
@@ -1,6 +1,6 @@
 import { ButtonHTMLAttributes } from "react";
 import { type VariantProps } from "class-variance-authority";
-import { buttonCva } from "src/cva/button";
+import { buttonCva } from "./../../../cva/button";
 
 export interface ButtonProps
     extends ButtonHTMLAttributes<HTMLButtonElement>,
