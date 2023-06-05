55dcc03180e7a1bff02c9b05393239cf18f574f3
mauchaconrithmXO <Mauricio.Chacon@rithmXO.com>
2023-06-05 18:13:40 -0400

    fix: remove config radius and fix button

--- packages/ui/ui-latamwin/src/cva/button/variantSize.ts
+++ packages/ui/ui-latamwin/src/cva/button/variantSize.ts
@@ -1,23 +1,5 @@
 export const variantSizes = {
-    sm: [
-        "py-[8px]",
-        "px-[18px]",
-        "rounded-[8px]",
-        "leading-[14.06px]",
-        "text-xs",
-    ],
-    md: [
-        "py-[8px]",
-        "px-[32px]",
-        "rounded-[8px]",
-        "leading-[23.44px]",
-        "text-xl",
-    ],
-    lg: [
-        "py-[14px]",
-        "px-[32px]",
-        "rounded-[12px]",
-        "leading-[23.44px]",
-        "text-xl",
-    ],
+    sm: ["py-2", "px-[18px]", "rounded-lg", "leading-[14.06px]", "text-xs"],
+    md: ["py-2", "px-8", "rounded-lg", "leading-[23.44px]", "text-xl"],
+    lg: ["py-3.5", "px-8", "rounded-xl", "leading-[23.44px]", "text-xl"],
 };
