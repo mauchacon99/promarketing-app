6ae8f18a7a692811c6feeb4f4ec6dc3eb4c37fdc
Francisco Aranda <francisco.aranda@sudo.cl>
2023-06-07 12:11:53 -0400

    chore: updated storybook preview config to follow latest standards

--- apps/docs/ui-latamwin/.storybook/preview.tsx
+++ apps/docs/ui-latamwin/.storybook/preview.tsx
@@ -1,33 +1,42 @@
+import React, { useEffect } from "react";
+import { Preview } from "@storybook/react";
+
 import { DEFAULT_THEME, withTailwindTheme } from "./decorator";
 import "ui-latamwin/src/styles.scss";
 import "./../styles.scss";
 
-export const parameters = {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-        matchers: {
-            color: /(background|color)$/i,
-            date: /Date$/,
+const preview: Preview = {
+    parameters: {
+        actions: { argTypesRegex: "^on[A-Z].*" },
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/,
+            },
         },
     },
-};
-
-export const globalTypes = {
-    theme: {
-        name: "Themes",
-        description: "Global theme for skin",
-        defaultValue: DEFAULT_THEME,
-        toolbar: {
-            icon: "paintbrush",
-            // Array of plain string values or MenuItem shape (see below)
-            items: [
-                { value: "winchile", title: "Winchile", left: "🌞" },
-                { value: "casinoenchile", title: "CasinoEnChile", left: "🌛" },
-            ],
-            // Change title based on selected value
-            dynamicTitle: true,
+    globalTypes: {
+        theme: {
+            name: "Themes",
+            description: "Global theme for skin",
+            defaultValue: DEFAULT_THEME,
+            toolbar: {
+                icon: "paintbrush",
+                // Array of plain string values or MenuItem shape (see below)
+                items: [
+                    { value: "winchile", title: "Winchile", left: "🌞" },
+                    {
+                        value: "casinoenchile",
+                        title: "CasinoEnChile",
+                        left: "🌛",
+                    },
+                ],
+                // Change title based on selected value
+                dynamicTitle: true,
+            },
         },
     },
+    decorators: [withTailwindTheme],
 };
 
-export const decorators = [withTailwindTheme];
+export default preview;
