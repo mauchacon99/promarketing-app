25618ae7f5e46e15aec7014f11a675678cc06745
roblesdotdev <httpsnip@gmail.com>
2023-08-02 11:14:53 -0300

    fix: add suggested changes

--- packages/ui/ui-latamwin/src/components/ui/badge/badge.test.tsx
+++ packages/ui/ui-latamwin/src/components/ui/badge/badge.test.tsx
@@ -1,40 +1,43 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import Badge from "./badge";
 import { cx } from "class-variance-authority";
+import Badge from "./badge";
 import classNameVariantColorScheme from "@cva/ui/badge/classNameVariantColorScheme";
 import classNameVariantVariety from "@cva/ui/badge/classNameVariantVariety";
 
-const testRootId = "badge-id";
-const testContent = "1";
-
 describe("BadgeComponent", () => {
-    it(`should render a badge with the text ${testContent}`, () => {
-        render(<Badge data-testid={testRootId} content={testContent} />);
-        const rendered = screen.getByTestId(testRootId);
-        expect(rendered).toHaveTextContent(testContent);
+    it("should render a badge with the text '1'", () => {
+        render(<Badge data-testid="badge-id" content={1} />);
+        const rendered = screen.getByTestId("badge-id");
+        expect(rendered).toHaveTextContent("1");
     });
-    it(`should render a badge with the svg children`, () => {
+    it("should render a badge with the svg children", () => {
         render(
-            <Badge data-testid={testRootId} content={testContent}>
+            <Badge data-testid="badge-id" content={1}>
                 <svg aria-label="icon" />
             </Badge>,
         );
         expect(screen.getByLabelText("icon")).toBeInTheDocument();
     });
-
+    it("should render a badge with the correct variety", () => {
+        render(
+            <Badge data-testid="badge-id" content={"123"} variety="single" />,
+        );
+        const rendered = screen.getByTestId("badge-id");
+        expect(rendered).toHaveClass(cx(classNameVariantVariety.multidigit));
+    });
     describe("should render a badge with the color scheme primary", () => {
         it("should render with the variety single", () => {
             render(
                 <Badge
-                    data-testid={testRootId}
+                    data-testid="badge-id"
                     colorScheme="primary"
-                    content={testContent}
+                    content={1}
                     variety="single"
                 />,
             );
-            const rendered = screen.getByTestId(testRootId);
+            const rendered = screen.getByTestId("badge-id");
             expect(rendered).toHaveClass(
                 cx(classNameVariantColorScheme.primary),
             );
@@ -43,13 +46,13 @@ describe("BadgeComponent", () => {
         it("should render with the variety multidigit", () => {
             render(
                 <Badge
-                    data-testid={testRootId}
+                    data-testid="badge-id"
                     colorScheme="primary"
-                    content={testContent}
+                    content={12}
                     variety="multidigit"
                 />,
             );
-            const rendered = screen.getByTestId(testRootId);
+            const rendered = screen.getByTestId("badge-id");
             expect(rendered).toHaveClass(
                 cx(classNameVariantVariety.multidigit),
             );
@@ -57,13 +60,13 @@ describe("BadgeComponent", () => {
         it("should render with the variety digit", () => {
             render(
                 <Badge
-                    data-testid={testRootId}
+                    data-testid="badge-id"
                     colorScheme="primary"
-                    content={testContent}
+                    content={1}
                     variety="digit"
                 />,
             );
-            const rendered = screen.getByTestId(testRootId);
+            const rendered = screen.getByTestId("badge-id");
             expect(rendered).toHaveClass(cx(classNameVariantVariety.digit));
         });
     });
@@ -71,26 +74,26 @@ describe("BadgeComponent", () => {
         it("should render with the variety single", () => {
             render(
                 <Badge
-                    data-testid={testRootId}
+                    data-testid="badge-id"
                     colorScheme="error"
-                    content={testContent}
+                    content={1}
                     variety="single"
                 />,
             );
-            const rendered = screen.getByTestId(testRootId);
+            const rendered = screen.getByTestId("badge-id");
             expect(rendered).toHaveClass(cx(classNameVariantColorScheme.error));
             expect(rendered).toHaveClass(cx(classNameVariantVariety.single));
         });
         it("should render with the variety multidigit", () => {
             render(
                 <Badge
-                    data-testid={testRootId}
+                    data-testid="badge-id"
                     colorScheme="primary"
-                    content={testContent}
+                    content={12}
                     variety="multidigit"
                 />,
             );
-            const rendered = screen.getByTestId(testRootId);
+            const rendered = screen.getByTestId("badge-id");
             expect(rendered).toHaveClass(
                 cx(classNameVariantVariety.multidigit),
             );
@@ -98,13 +101,13 @@ describe("BadgeComponent", () => {
         it("should render with the variety digit", () => {
             render(
                 <Badge
-                    data-testid={testRootId}
+                    data-testid="badge-id"
                     colorScheme="primary"
-                    content={testContent}
+                    content={1}
                     variety="digit"
                 />,
             );
-            const rendered = screen.getByTestId(testRootId);
+            const rendered = screen.getByTestId("badge-id");
             expect(rendered).toHaveClass(cx(classNameVariantVariety.digit));
         });
     });
