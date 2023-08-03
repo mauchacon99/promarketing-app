4fff0e621aac9a5668f573e59d6eb972e112d9eb
roblesdotdev <httpsnip@gmail.com>
2023-08-03 13:21:20 -0300

    test: add missing tests

--- packages/ui/ui-latamwin/src/components/ui/input/input.test.tsx
+++ packages/ui/ui-latamwin/src/components/ui/input/input.test.tsx
@@ -38,18 +38,76 @@ describe("InputComponent", () => {
     });
 
     describe("Input ui helpers", () => {
+        it("should render input with placeholder", () => {
+            const { getByPlaceholderText } = render(
+                <Input
+                    data-testid="test-input"
+                    placeholder="test placeholder"
+                />,
+            );
+            const inputElement = getByPlaceholderText("test placeholder");
+
+            expect(inputElement).toHaveAttribute(
+                "placeholder",
+                "test placeholder",
+            );
+        });
+
         it("should render with bottom line", () => {
             const { getAllByText } = render(
                 <Input data-testid="test-input" hasBottomLine />,
             );
 
-            // TODO: improve this test
             const allSpans = getAllByText(
                 (_, element) => element?.tagName.toLowerCase() === "span",
             );
 
             expect(allSpans[0]).toHaveClass(
                 inputBottomLineCva({ colorScheme: "default" }),
+            );
+        });
+
+        it("should not render bottom line when is disabled", () => {
+            const { container } = render(
+                <Input data-testid="test-input" hasBottomLine disabled />,
+            );
+
+            expect(container.querySelector("span")).not.toBeTruthy();
+        });
+
+        it("should render with bottom line with success color scheme", () => {
+            const { getAllByText } = render(
+                <Input
+                    data-testid="test-input"
+                    hasBottomLine
+                    colorScheme="success"
+                />,
+            );
+
+            const allSpans = getAllByText(
+                (_, element) => element?.tagName.toLowerCase() === "span",
+            );
+
+            expect(allSpans[0]).toHaveClass(
+                inputBottomLineCva({ colorScheme: "success" }),
+            );
+        });
+
+        it("should render with bottom line with error color scheme", () => {
+            const { getAllByText } = render(
+                <Input
+                    data-testid="test-input"
+                    hasBottomLine
+                    colorScheme="error"
+                />,
+            );
+
+            const allSpans = getAllByText(
+                (_, element) => element?.tagName.toLowerCase() === "span",
+            );
+
+            expect(allSpans[0]).toHaveClass(
+                inputBottomLineCva({ colorScheme: "error" }),
             );
         });
 
@@ -60,20 +118,54 @@ describe("InputComponent", () => {
             expect(getByText("help")).toBeTruthy();
         });
 
-        it("should display left element", () => {
-            const el = <svg data-testid="left-icon" />;
+        it("should display left element with correct classes", () => {
+            const element = <svg data-testid="left-icon" />;
             const { queryByTestId } = render(
-                <Input data-testid="test-input" leftElement={el} />,
+                <Input
+                    data-testid="test-input"
+                    leftElement={element}
+                    leftElementClasses="w-4 h-4"
+                />,
             );
+            const leftElement = queryByTestId("left-icon");
+            expect(leftElement).toBeTruthy();
+            expect(leftElement?.parentElement).toHaveClass("w-4 h-4");
+        });
+
+        it("should display right element with correct classes", () => {
+            const element = <svg data-testid="right-icon" />;
+            const { queryByTestId } = render(
+                <Input
+                    data-testid="test-input"
+                    rightElement={element}
+                    rightElementClasses="w-4 h-4"
+                />,
+            );
+            const rightElement = queryByTestId("right-icon");
+            expect(rightElement).toBeTruthy();
+            expect(rightElement?.parentElement).toHaveClass("w-4 h-4");
+        });
+
+        it("should display right and left elements", () => {
+            const rightElement = <svg data-testid="right-icon" />;
+            const leftElement = <svg data-testid="left-icon" />;
+            const { queryByTestId } = render(
+                <Input
+                    data-testid="test-input"
+                    rightElement={rightElement}
+                    leftElement={leftElement}
+                />,
+            );
+            expect(queryByTestId("right-icon")).toBeTruthy();
             expect(queryByTestId("left-icon")).toBeTruthy();
         });
 
-        it("should display right element", () => {
-            const el = <svg data-testid="right-icon" />;
-            const { queryByTestId } = render(
-                <Input data-testid="test-input" rightElement={el} />,
+        it("should not display left and right elements", () => {
+            const { container } = render(
+                <Input data-testid="test-input" hasBottomLine disabled />,
             );
-            expect(queryByTestId("right-icon")).toBeTruthy();
+
+            expect(container.querySelectorAll("span").length).toBe(0);
         });
     });
 });
